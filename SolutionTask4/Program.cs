764e0064e57d1a1e9fedd95078731874dae1148e
Yaroslav Evstifeev <krakurs@yandex.ru>
2022-07-13 21:03:24 +0300

    Add SolutionTask4

---
+++ SolutionTask4/Program.cs
@@ -0,0 +1,11 @@
+﻿string? inputLine = Console.ReadLine();
+
+if(inputLine != null)
+{
+    int inputNamber = int.Parse(inputLine);
+
+    int threeNamber = inputNamber%10;
+
+    Console.WriteLine(threeNamber);
+
+}
