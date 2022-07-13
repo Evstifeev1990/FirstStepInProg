401a3a7223f04a392c5e562576e809e99331acab
Yaroslav Evstifeev <krakurs@yandex.ru>
2022-07-13 17:36:42 +0300

    Add SolutionTask3

---
+++ SolutionTask3/Program.cs
@@ -0,0 +1,22 @@
+﻿string? inputLine = Console.ReadLine();
+
+if(inputLine != null)
+{
+    int inputNamber = int.Parse(inputLine);
+
+    int minusNamber = inputNamber * -1;
+
+    string outputLine = "";
+
+    while(minusNamber < inputNamber)
+    {
+        outputLine = outputLine + minusNamber + ", ";
+
+        minusNamber++;
+    }
+
+    outputLine = outputLine + inputNamber;
+
+    Console.Write(outputLine);
+
+}   
