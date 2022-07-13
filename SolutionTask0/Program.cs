b2bce28f687bd9357b8851cfae9373b5a6e4a781
Yaroslav Evstifeev <krakurs@yandex.ru>
2022-07-13 13:55:34 +0300

    Add SolutionTask0

--- SolutionTask0/Program.cs
+++ SolutionTask0/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string? inputLine = Console.ReadLine();
+
+if(inputLine != null)
+{
+    int inputNamber = int.Parse(inputLine);
+
+    //int outNamber = inputNamber * inputNamber;
+    int outNamber = (int)Math.Pow(inputNamber, 2);
+
+    Console.WriteLine(outNamber);
+}
