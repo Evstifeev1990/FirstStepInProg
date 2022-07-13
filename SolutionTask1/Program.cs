6affe8dcc0262ea91dab6eb11095538af55ef03d
Yaroslav Evstifeev <krakurs@yandex.ru>
2022-07-13 14:34:11 +0300

    Add SolutionTask1

---
+++ SolutionTask1/Program.cs
@@ -0,0 +1,17 @@
+﻿string? InputLine1 = Console.ReadLine();
+string? InputLine2 = Console.ReadLine();
+
+if (InputLine1 != null && InputLine2 != null)
+{
+    int inputNamber1 = int.Parse(InputLine1);
+    int inputNamber2 = int.Parse(InputLine2);
+
+    if (inputNamber1 == Math.Pow(inputNamber2, 2))
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}
