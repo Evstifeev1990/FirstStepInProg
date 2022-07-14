c9995fb13ef79955fd2ea18e750a85449d7d151b
Yaroslav Evstifeev <krakurs@yandex.ru>
2022-07-14 09:46:34 +0300

    Add SolutionTask7

---
+++ SolutionTask7/Program.cs
@@ -0,0 +1,16 @@
+﻿string? inputLine = Console.ReadLine();
+
+if(inputLine != null)
+{
+    int inputNamber = int.Parse(inputLine);
+
+    if(inputNamber%2 == 0)
+    {
+       Console.WriteLine("Число чётное"); 
+    }
+    else
+    {
+       Console.WriteLine("Число нечётное");
+    }
+    
+}
