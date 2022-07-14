d15ce5ae3e8de2fe58fa39b99c2503199058245c
Yaroslav Evstifeev <krakurs@yandex.ru>
2022-07-14 09:04:42 +0300

    Add SolutionTask5

---
+++ SolutionTask5/Program.cs
@@ -0,0 +1,23 @@
+﻿string? InputLine1 = Console.ReadLine();
+string? InputLine2 = Console.ReadLine();
+
+if (InputLine1 != null && InputLine2 != null)
+{
+    int inputNamber1 = int.Parse(InputLine1);
+    int inputNamber2 = int.Parse(InputLine2);
+
+    if (inputNamber1 > inputNamber2)
+    {
+        Console.WriteLine("Первое число больше второго");
+    }
+    if (inputNamber1 < inputNamber2)
+    {
+        Console.WriteLine("Второе число больше первого");
+    }
+    if (inputNamber1 == inputNamber2)
+    {
+        Console.WriteLine("Оба числа равны");
+    }
+    
+
+}
