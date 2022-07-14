fadf6e02e44a06cc401a14b6e954f5a4cbb1c578
Yaroslav Evstifeev <krakurs@yandex.ru>
2022-07-14 10:13:25 +0300

    Add SolutionTask8

---
+++ SolutionTask8/Program.cs
@@ -0,0 +1,29 @@
+﻿string? inputLine = Console.ReadLine();
+
+if(inputLine != null)
+{
+    int inputNamber = int.Parse(inputLine);
+
+    int zeroNamber = 2;
+
+    string outputLine = "";
+
+    while(zeroNamber < inputNamber)
+    {
+        if(zeroNamber%2 == 0)
+        {
+            outputLine = outputLine + zeroNamber + ", ";
+        }
+
+        zeroNamber++;
+    }
+
+    if(zeroNamber%2 == 0)
+    {
+        outputLine = outputLine + zeroNamber;
+    }
+
+
+    Console.Write(outputLine);
+
+}   
