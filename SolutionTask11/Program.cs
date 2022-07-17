dd58c9d43f2c695e52990d0cb1bcb4accfe14742
Yaroslav Evstifeev <krakurs@yandex.ru>
2022-07-18 02:19:12 +0300

    Add SolutionTask11

---
+++ SolutionTask11/Program.cs
@@ -0,0 +1,23 @@
+﻿string? InputLine1 = Console.ReadLine();
+string? InputLine2 = Console.ReadLine();
+
+if (InputLine1 != null && InputLine2 != null)
+{
+    int inputNamber1 = int.Parse(InputLine1);
+    int inputNamber2 = int.Parse(InputLine2);
+    int inputNamber3 = inputNamber1%inputNamber2;
+
+    if(inputNamber1%inputNamber2 == 0)
+    {
+        Console.WriteLine("Кратно");
+    }
+    
+    else
+    {
+        Console.WriteLine(inputNamber3);
+    }
+    
+        
+    
+    
+}
