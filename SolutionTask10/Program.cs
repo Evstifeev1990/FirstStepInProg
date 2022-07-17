720c4447f7cdfafd05a3f86dc10b0e2e4dd82cc3
Yaroslav Evstifeev <krakurs@yandex.ru>
2022-07-18 01:58:10 +0300

    Add SolutionTask10

---
+++ SolutionTask10/Program.cs
@@ -0,0 +1,31 @@
+﻿System.Random numberSintezator = new Random();
+
+void noldva(System.Random numberSintezator)
+{
+    int index = numberSintezator.Next(100, 1000);
+
+    Console.WriteLine(index);
+
+    string stroka = index.ToString();
+
+
+    Console.Write(stroka[0]);
+    Console.Write(stroka[2]);
+}
+
+noldva(numberSintezator);
+
+// int oneNamber = index / 10;
+
+// int twoNamber = index % 10;
+
+// Console.WriteLine(index);
+
+// if (oneNamber > twoNamber)
+// {
+//     Console.WriteLine(oneNamber);
+// }
+// else
+// {
+//     Console.WriteLine(twoNamber);
+// }
