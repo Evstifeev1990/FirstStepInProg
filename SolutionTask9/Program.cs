fed63909cf31cae429b3e5895bb456342844fee0
Yaroslav Evstifeev <krakurs@yandex.ru>
2022-07-18 01:24:34 +0300

    Add SolutionTask9

---
+++ SolutionTask9/Program.cs
@@ -0,0 +1,25 @@
+﻿System.Random numberSintezator = new Random();
+
+//метод решения задачи вариянт1
+void variant1(System.Random numberSintezator)
+{
+    int index = numberSintezator.Next(10, 100);
+
+    int oneNamber = index / 10;
+
+    int twoNamber = index % 10;
+
+    Console.WriteLine(index);
+
+    if (oneNamber > twoNamber)
+    {
+        Console.WriteLine(oneNamber);
+    }
+    else
+    {
+        Console.WriteLine(twoNamber);
+    }
+}
+
+variant1(numberSintezator);
+
