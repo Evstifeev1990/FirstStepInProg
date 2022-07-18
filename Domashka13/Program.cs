e547e6b274a7fd6f8e2917ea4ce450a2b8daf420
Yaroslav Evstifeev <krakurs@yandex.ru>
2022-07-18 13:34:26 +0300

    Add Domashka13

---
+++ Domashka13/Program.cs
@@ -0,0 +1,41 @@
+﻿string? inputLine = Console.ReadLine();
+
+if (inputLine != null)
+{
+    // первый вариант
+    void Tri(string vvod)
+    {
+        int inputNamber = int.Parse(vvod);
+
+        if (inputNamber > 99)
+        {
+            // Переводим число в строку
+            string stroka = inputNamber.ToString();
+            Console.WriteLine(stroka[2]);
+        }
+        else
+        {
+            Console.WriteLine("Третьего числа нет");
+        }
+
+
+    }
+
+    
+    // второй вариант
+    void Tri2(string vvod1)
+    {
+        try
+        {
+            Console.Write(vvod1[2]);
+        }
+        // ловим ошибку
+        catch
+        {
+            Console.WriteLine("Третьего числа нет");
+        }
+    }
+
+    Tri(inputLine);
+    Tri2(inputLine);
+}
