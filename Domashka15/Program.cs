f150c8d32036f3a1b0cf6afddb7f149f706b6d02
Yaroslav Evstifeev <krakurs@yandex.ru>
2022-07-18 13:57:36 +0300

    Add Domashka15

---
+++ Domashka15/Program.cs
@@ -0,0 +1,37 @@
+﻿string? inputLine = Console.ReadLine();
+
+if(inputLine != null)
+{
+    // вариант первый
+    void vykhodnoy(string den)
+    {
+       int inputDen = int.Parse(den);
+
+       if (inputDen == 6 || inputDen == 7)
+       {
+            Console.WriteLine("Да");
+       } 
+       else
+       {
+            Console.WriteLine("Нет");
+       }
+    }
+    
+    vykhodnoy(inputLine);
+    
+     void vykhodnoy1(string den)
+    {
+       int inputDen = int.Parse(den);
+
+       if (inputDen / 6 == 1 || inputDen / 7 == 1)
+       {
+            Console.WriteLine("Да");
+       } 
+       else
+       {
+            Console.WriteLine("Нет");
+       }
+    }
+
+    vykhodnoy1(inputLine);
+}
