08011da49cca31d57a6a76aff5a86d6a7ea0efe1
Yaroslav Evstifeev <krakurs@yandex.ru>
2022-07-18 10:11:53 +0300

    Add SolutionTask12

---
+++ SolutionTask12/Program.cs
@@ -0,0 +1,19 @@
+﻿string? InputLine1 = Console.ReadLine();
+
+if (InputLine1 != null)
+{
+    int inputNamber1 = int.Parse(InputLine1);
+
+
+    if(inputNamber1%23 == 0 && inputNamber1%7 == 0)
+    {
+        Console.WriteLine("Кратно");
+    }
+    
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+        
+}
+
