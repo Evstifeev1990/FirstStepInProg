5eda1a4e7d7249a7690a8db70627c9663bf284d5
Yaroslav Evstifeev <krakurs@yandex.ru>
2022-07-25 12:50:21 +0300

    Add SolutionTask22

---
+++ SolutionTask22/Program.cs
@@ -0,0 +1,27 @@
+﻿string? inputLine = Console.ReadLine();
+
+if(inputLine != null)
+{
+    int namberN = int.Parse(inputLine);
+
+    string lineNone = string.Empty;
+
+    string lineNtwo = string.Empty;
+
+    int s = 0;
+
+    while(s <= namberN)
+    {
+        lineNone = lineNone + s + " ";
+
+        lineNtwo = lineNtwo + Math.Pow(s, 2) + " ";
+
+        s++;
+    }
+
+    // outputLine = outputLine + inputNamber;
+
+    Console.WriteLine(lineNone);
+    Console.WriteLine(lineNtwo);
+
+}   
