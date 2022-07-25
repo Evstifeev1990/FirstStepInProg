4f31ba7e201d28eeaece79eb6e351cf6d6fbeb4d
Yaroslav Evstifeev <krakurs@yandex.ru>
2022-07-25 13:02:01 +0300

    Add Domashka23

---
+++ Domashka23/Program.cs
@@ -0,0 +1,27 @@
+﻿//Напишите программу, которая принимает на вход число (N) 
+//и выдаёт таблицу кубов чисел от 1 до N.
+
+string? inputLine = Console.ReadLine();
+
+if(inputLine != null)
+{
+    int namberN = int.Parse(inputLine);
+
+    string lineNcub = string.Empty;
+
+    int s = 1;
+
+    //цикл нахождения кубов чисел от 1 до введённого числа
+    while(s <= namberN)
+    {
+        lineNcub = lineNcub + Math.Pow(s, 3) + " ";
+
+        s++;
+    }
+
+    
+
+  // вывод результата
+    Console.WriteLine(lineNcub);
+
+}   
