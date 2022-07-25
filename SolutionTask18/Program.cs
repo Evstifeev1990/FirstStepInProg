9f27fa578edae80e3b11b373a9b37e608311f833
Yaroslav Evstifeev <krakurs@yandex.ru>
2022-07-25 09:58:23 +0300

    Add SolutionTask18

---
+++ SolutionTask18/Program.cs
@@ -0,0 +1,18 @@
+﻿void printAnswer(int namber)
+{
+    if(namber == 1) Console.WriteLine("Допустимо: x>0, y>0");
+    if(namber == 2) Console.WriteLine("Допустимо: x<0, y>0");
+    if(namber == 3) Console.WriteLine("Допустимо: x<0, y<0");
+    if(namber == 4) Console.WriteLine("Допустимо: x>0, y<0");
+
+}
+
+string? inputLine = Console.ReadLine();
+
+if(inputLine != null)
+{
+    int inputNamber = int.Parse(inputLine);
+
+    printAnswer(inputNamber);
+
+}
