f226979fe8f887a85ce89ec69b136c263a6627e3
Yaroslav Evstifeev <krakurs@yandex.ru>
2022-08-05 13:39:10 +0300

    Add SolutionTask24

---
+++ SolutionTask24/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число: ");
+
+int imputNamber = int.Parse(Console.ReadLine());
+
+void VariantSimple()
+{
+    int sumOfNumbers = 0;
+
+    for(int i = 1; i <= imputNamber; i++)
+    {
+        sumOfNumbers += i;
+    }
+Console.WriteLine("Сумма чисел от 1 до "+ imputNamber + " = "+ sumOfNumbers);
+}
+VariantSimple();
