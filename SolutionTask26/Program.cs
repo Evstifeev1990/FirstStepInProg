fd7eceecd6bb2dbeeb13a3d9f0185dd65b04b3b0
Yaroslav Evstifeev <krakurs@yandex.ru>
2022-08-07 10:56:34 +0300

    Add SolutionTask26

---
+++ SolutionTask26/Program.cs
@@ -0,0 +1,50 @@
+﻿Console.Write("Введите число: ");
+
+string inputLineNamber = Console.ReadLine() ?? "";
+int imputNamber = int.Parse(inputLineNamber);
+
+DateTime timePoint = DateTime.Now;
+
+int VariantChar()
+{
+    // int namberLength = 0;
+    // char[] array = inputLineNamber.ToCharArray();
+    // namberLength = array.Length;
+
+    return inputLineNamber.ToCharArray().Length;
+}
+
+int VariantSimple()
+{
+    int namberLength = 0;
+    int digits = 1;
+    while(digits <= imputNamber)
+    {
+        digits = digits*10;
+        namberLength++;
+    }
+    return namberLength;
+}
+
+int VariantLog10()
+{
+    return (int)Math.Log10(imputNamber)+1;
+}
+
+int result = 0;
+
+timePoint = DateTime.Now;
+result = VariantChar();
+Console.WriteLine(result);
+Console.WriteLine(DateTime.Now - timePoint);
+
+timePoint = DateTime.Now;
+result = VariantSimple();
+Console.WriteLine(result);
+Console.WriteLine(DateTime.Now - timePoint);
+
+timePoint = DateTime.Now;
+result = VariantLog10();
+Console.WriteLine(result);
+Console.WriteLine(DateTime.Now - timePoint);
+
