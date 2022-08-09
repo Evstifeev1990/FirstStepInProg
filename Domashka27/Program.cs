c1476d3185ce167c2b676399199ad66742090441
Yaroslav Evstifeev <krakurs@yandex.ru>
2022-08-09 12:42:57 +0300

    Add Domashka27

---
+++ Domashka27/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.Write("Введите число: ");
+string? inputLine = Console.ReadLine() ?? "";
+int inputNum = int.Parse(inputLine);
+DateTime timePoint = DateTime.Now;
+
+//подскажите как решить методом перевода в массив???
+void MetodNaiv()
+{
+    int sum = 0;
+    while (inputNum>0)
+    {
+        sum = sum + inputNum%10;
+        inputNum = inputNum/10;
+    }
+    Console.WriteLine("Сумма чисел данного числа = "+sum);
+
+}
+
+timePoint = DateTime.Now;
+MetodNaiv();
+Console.WriteLine(DateTime.Now - timePoint);
