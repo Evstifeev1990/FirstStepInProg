fdf3b470b015d59e73f870a9a897d93777683d8f
Yaroslav Evstifeev <krakurs@yandex.ru>
2022-08-12 15:39:41 +0300

    Domashka36

---
+++ Domashka36/Program.cs
@@ -0,0 +1,57 @@
+﻿//медот заполнения массива
+int[] FillingArray()
+{
+    //указываем длинну массива
+    int[] outArray = new int[15];
+    int i = 0;
+    //вводим рандомную переменную
+    System.Random namberSintezator = new System.Random();
+    //цикл длины массива
+    while(i<15)
+    {
+        //присвоение  числа массиву
+        outArray[i] = namberSintezator.Next(10, 100);
+        i++;
+    }
+    return outArray;
+}
+
+//медот вывода массива на экран
+void PrintInArray(int[] inputArray)
+{
+    int i =0;
+    int n = inputArray.Length;
+    while(i<(n-1))
+    {
+        Console.Write(inputArray[i]+", ");
+        i++;
+    }
+    Console.WriteLine(inputArray[i]);
+}
+
+//метод нахождения суммы нечётных элементов массива
+int ColculateTask(int[] inputArray)
+{
+    //ввод переменной количества чётных чисел
+    int resultSearch = 0;
+    int i = 0;
+    //цикл по длине массива
+    while(i<15)
+    {
+        //проверкаи на условие нечётности элемента
+        if(i%2 == 1) 
+        {
+            resultSearch = resultSearch+inputArray[i];
+            
+        }
+        i++;
+    }
+    return resultSearch;
+}
+
+//формирование массива
+int[] bufferArray = FillingArray();
+//вывод массива
+PrintInArray(bufferArray);
+//вывод количества чётных чисел
+Console.WriteLine("сумма чисел нечётных позиций в массиве = "+ColculateTask(bufferArray));
