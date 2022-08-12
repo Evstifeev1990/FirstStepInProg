d6081d0bd8cc23e00cde5a06695e94a4b0b8a1a8
Yaroslav Evstifeev <krakurs@yandex.ru>
2022-08-12 10:27:34 +0300

    SolutionTask32

---
+++ SolutionTask32/Program.cs
@@ -0,0 +1,51 @@
+﻿int[] FillingArray()
+{
+    int[] outArray = new int[12];
+    int i = 0;
+    System.Random namberSintezator = new System.Random();
+    while(i<12)
+    {
+        outArray[i] = namberSintezator.Next(-9, 10);
+        i++;
+    }
+    return outArray;
+}
+
+void PrintInArray(int[] inputArray)
+{
+    int i =0;
+    int n = inputArray.Length;
+    while(i<(n-1))
+    {
+        Console.Write(inputArray[i]+", ");
+        i++;
+    }
+    Console.WriteLine(inputArray[i]);
+}
+
+void ColculateTask(int[] outArry)
+{
+    int i = 0;
+    while(i<12)
+    {
+        outArry[i]=outArry[i]*(-1);
+        i++;
+    }
+}
+
+//  void PrintResult(int[] array)
+// {
+//     Console.WriteLine(array);
+// }
+
+void VariantNaiv()
+{
+    
+    int[] bufferArray = FillingArray();
+    PrintInArray(bufferArray);
+    ColculateTask(bufferArray);
+    PrintInArray(bufferArray);
+    // PrintResult(); 
+}
+
+VariantNaiv();
