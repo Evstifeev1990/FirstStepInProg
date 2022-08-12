f20a2d8d03ac90ad8935a36f4087d0386174f5bc
Yaroslav Evstifeev <krakurs@yandex.ru>
2022-08-12 15:23:08 +0300

    Domashka34

---
+++ Domashka34/Program.cs
@@ -0,0 +1,56 @@
+﻿//медот заполнения массива
+int[] FillingArray()
+{
+    //указываем длинну массива
+    int[] outArray = new int[10];
+    int i = 0;
+    //вводим рандомную переменную
+    System.Random namberSintezator = new System.Random();
+    //цикл длины массива
+    while(i<10)
+    {
+        //присвоение трёхзначного числа массиву
+        outArray[i] = namberSintezator.Next(100, 1000);
+        i++;
+    }
+    return outArray;
+}
+
+//медот вывода массива на экран
+void PrintInArray(int[] inputArray)
+{
+    int i =0;
+    int n = inputArray.Length;
+    while(i<(n-1))
+    {
+        Console.Write(inputArray[i]+", ");
+        i++;
+    }
+    Console.WriteLine(inputArray[i]);
+}
+
+//метод нахождения числа
+int ColculateTask(int[] inputArray)
+{
+    //ввод переменной количества чётных чисел
+    int resultSearch = 0;
+    int i = 0;
+    //цикл по длине массива
+    while(i<10)
+    {
+        //проверкаи на условие чётности
+        if(inputArray[i]%2 == 0) 
+        {
+            resultSearch++;
+        }
+        i++;
+    }
+    return resultSearch;
+}
+
+//формирование массива
+int[] bufferArray = FillingArray();
+//вывод массива
+PrintInArray(bufferArray);
+//вывод количества чётных чисел
+Console.WriteLine("Количество чётных чисел в массиве = "+ColculateTask(bufferArray));
