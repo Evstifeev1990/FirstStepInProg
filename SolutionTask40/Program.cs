162e82e8cd88f1c3e9a94a6c2d18d886ab0c0ef9
Yaroslav Evstifeev <krakurs@yandex.ru>
2022-08-16 18:29:04 +0300

    SolutionTask40

---
+++ SolutionTask40/Program.cs
@@ -0,0 +1,39 @@
+﻿int sideA = 0;
+int sideB = 0;
+int sideC = 0;
+
+ReadSides();
+bool res = TestTriangle(sideA,sideB,sideC);
+PrintAnswer(res);
+
+void ReadSides()
+{
+    Console.Write("Введите первое число");
+    sideA = int.Parse(Console.ReadLine()??"");
+    Console.Write("Введите второе число");
+    sideB = int.Parse(Console.ReadLine()??"");
+    Console.Write("Введите третье число");
+    sideC = int.Parse(Console.ReadLine()??"");
+}
+
+bool TestTriangle(int num1, int num2, int num3)
+{
+    bool answer = ((num1+num2>num3)
+    &&(num1+num3>num2)
+    &&(num2+num3>num1))? true : false;
+    return answer; 
+}
+
+void PrintAnswer(bool answer)
+{
+    if (answer)
+    {
+        Console.Write("Из этих отрезков можно составить треугольник");
+    }
+    else
+    {
+        Console.Write("Из этих отрезков нельзя составить треугольник");
+    }
+    
+}
+
