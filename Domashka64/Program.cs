3d929c800e8f460fc4115485d57a1dd5bffa0560
Yaroslav Evstifeev <krakurs@yandex.ru>
2022-08-26 17:31:16 +0300

    Domashka64

---
+++ Domashka64/Program.cs
@@ -0,0 +1,19 @@
+﻿
+int ReadData(string line)
+{
+    Console.WriteLine(line);
+    return int.Parse(Console.ReadLine());
+}
+
+int namberM = ReadData("Задайте число М");
+int namberN = ReadData("Задайте число N");
+
+void NaturalNamber(int m, int n)
+{
+    if (m - 1 == n) return;
+    Console.Write(m + " ");
+    NaturalNamber(m + 1, n);
+}
+
+if (namberM < namberN) NaturalNamber(namberM, namberN);
+else NaturalNamber(namberN, namberM);
