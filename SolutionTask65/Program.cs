437c4e0207c2089c379318806c351dc7b350e4e3
Yaroslav Evstifeev <krakurs@yandex.ru>
2022-08-26 14:30:59 +0300

    add SolutionTask65

---
+++ SolutionTask65/Program.cs
@@ -0,0 +1,18 @@
+﻿
+int ReadData(string line)
+{
+    Console.WriteLine(line);
+    return int.Parse(Console.ReadLine());
+}
+
+int namberM = ReadData("Задайте число М");
+int namberN = ReadData("Задайте число N");
+
+void NaturalNamber(int m, int n)
+{
+    if (m - 1 == n) return;
+    Console.Write(m + " ");
+    NaturalNamber(m + 1, n);
+}
+
+bool test = namberM<namberN? NaturalNamber(namberM, namberN):NaturalNamber(namberN, namberM);
