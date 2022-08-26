ac7b3a0d254903dc9bad2219d193cb3af413559d
Yaroslav Evstifeev <krakurs@yandex.ru>
2022-08-26 13:40:10 +0300

    add SolutionTask63

---
+++ SolutionTask63/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+int ReadData()
+{
+    Console.WriteLine("Введите число: ");
+    return int.Parse(Console.ReadLine());
+}
+
+int NuturalNamber(int nam)
+{
+    if(nam == 2)return 1;
+    else
+    {
+        nam--;
+        Console.Write(NuturalNamber(nam));
+    }
+    return nam;
+}
+
+NuturalNamber(3);
