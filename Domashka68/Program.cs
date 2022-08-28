4462ba19ec9f72c823e55d4a10f7726a87b9ba23
Yaroslav Evstifeev <krakurs@yandex.ru>
2022-08-28 09:00:50 +0300

    Domashka68

---
+++ Domashka68/Program.cs
@@ -0,0 +1,13 @@
+﻿//реализация функции Аккермана Command+C и Comand+v очень сложно((((()))))
+tatic uint A(uint n, uint m)
+{
+  if (n == 0)
+    return m + 1;
+  else
+    if ((n != 0) && (m == 0))
+      return A(n - 1, 1);
+    else
+      return A(n - 1, A(n, m - 1));
+}
+
+Console.WriteLine(A(2,2));
