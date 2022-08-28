3ed22e04ed93b9e5f9742ca0caa9fc6daf11c8b1
Yaroslav Evstifeev <krakurs@yandex.ru>
2022-08-28 08:53:54 +0300

    Domashka66

---
+++ Domashka66/Program.cs
@@ -0,0 +1,25 @@
+﻿
+int ReadData(string line)
+{
+    Console.WriteLine(line);
+    return int.Parse(Console.ReadLine());
+}
+
+int namberM = ReadData("Задайте число М");
+int namberN = ReadData("Задайте число N");
+
+int sum = 0;
+
+void SumNaturalNamber(int m, int n)
+{
+    if (m - 1 == n)  
+    {
+        Console.WriteLine("Сумма всех элементов = "+sum);
+        return;
+    }
+    sum = sum+m;
+    SumNaturalNamber(m + 1, n);
+}
+
+if (namberM < namberN) SumNaturalNamber(namberM, namberN);
+else SumNaturalNamber(namberN, namberM);
